b7da4c7f490777a126c9b6cc09fd9f71fb117511
Grimnir03 <44272203+Grimnir03@users.noreply.github.com>
2019-10-18 11:47:40 -0600

    Update game.js

--- game.js
+++ game.js
@@ -1,32 +1,5 @@
 const gameState = {}
 
-function preload() {
-  this.load.image('codey', 'https://raw.githubusercontent.com/Grimnir03/Grimnir03.github.io/master/backgrounds/New%20Piskel%20(1).png');
-}
-
-function create() {
-  gameState.codey = this.add.sprite(150, 200, 'codey');
-  gameState.cursors = this.input.keyboard.createCursorKeys();
-}
-
-function update() {
-  if (gameState.cursors.right.isDown) {
-    gameState.codey.x += 5;
-  }
-
-  if (gameState.cursors.left.isDown) {
-    gameState.codey.x -= 5;
-  }
-
-  if (gameState.cursors.down.isDown) {
-    gameState.codey.y += 5;
-  }
-
-  if (gameState.cursors.up.isDown) {
-    gameState.codey.y -= 5;
-  }
-}
-
 const config = {
   type: Phaser.AUTO,
   width: 1000,
